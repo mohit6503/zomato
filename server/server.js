6f57003d899eb786e82430b5c23c92f72c6ee7e4
mohit6503 <mohitkumar42003@gmail.com>
2025-07-21 00:32:29 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -26,7 +26,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:"https://zomato-3cc5.vercel.app",
+  credentials: true,
+}));
 app.use(express.json());
 
 
