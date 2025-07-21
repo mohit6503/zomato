2afa027cfac6529beb18a420e086cc31e9e0100c
mohit6503 <mohitkumar42003@gmail.com>
2025-07-21 20:47:21 +0530

    finall

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5000',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
-
+// // Routes
+app.get('/', (req, res) => {
+    res.send('✅ Zomoto Backend is Running!');
+  });
   app.use('/api/auth', authRoutes);
   app.use('/api/user', userRoutes);
   app.use('/api/restaurants', restaurantRoutes);
@@ -56,5 +56,5 @@ server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(` Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
