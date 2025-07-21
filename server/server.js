66ae8a0a91f2686687a07d16953dff63bf822eb1
mohit6503 <mohitkumar42003@gmail.com>
2025-07-21 08:46:37 +0530

    revert

--- server/server.js
+++ server/server.js
@@ -26,15 +26,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin:"https://zomato-3cc5.vercel.app",
-  credentials: true,
-}));
+app.use(cors({}));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("🍽️ Zomato backend is live!");
-});
+
   app.use('/api/auth', authRoutes);
   app.use('/api/user', userRoutes);
   app.use('/api/restaurants', restaurantRoutes);
