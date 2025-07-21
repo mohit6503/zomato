927f161a20d7ab5739fbd311413c896b3044ba64
mohit6503 <mohitkumar42003@gmail.com>
2025-07-21 09:07:39 +0530

    backend update

--- server/server.js
+++ server/server.js
@@ -26,7 +26,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors({}));
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5000',
+  credentials: true,
+}));
 app.use(express.json());
 
 
